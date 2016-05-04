a0a1a83cb962776012ea03cbf078a75d637d7312
Johan Nyström <johan_n694@hotmail.com>
2016-05-04 22:27:51 +0200

    passing example test

--- app/components/__tests__/HelloWorld.test.js
+++ app/components/__tests__/HelloWorld.test.js
@@ -1,3 +1,5 @@
+jest.unmock('../HelloWorld')
+
 import React from 'react'
 import ReactDOM from 'react-dom'
 import TestUtils from 'react-addons-test-utils'
@@ -5,9 +7,16 @@ import TestUtils from 'react-addons-test-utils'
 import HelloWorld from '../HelloWorld'
 
 const hello = TestUtils.renderIntoDocument(
-    <HelloWorld text={"World"} />
+    <div>
+        <HelloWorld text={"World"} />
+    </div>
 )
 
-const helloNode = ReactDOM.findDOMNode(hello)
+const helloNode = ReactDOM.findDOMNode(hello).children[0]
 
-expect(helloNode.textContent).toEqual('Hello World')
+describe('A sample test', () => {
+    it('works', () => {
+        expect(helloNode.textContent).toEqual('Hello World')
+    })
+})
+
