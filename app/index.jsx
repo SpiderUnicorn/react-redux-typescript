d44111ec0273e8cf5714d420595dc48298db6b13
Johan Nyström <johan_n694@hotmail.com>
2016-06-08 17:50:33 +0200

    refactored store configuration to its own file

--- app/index.jsx
+++ app/index.jsx
@@ -2,17 +2,11 @@ import React from 'react'
 import { Router, Route, browserHistory } from 'react-router'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import configure from './store/'
+import configure from './store/configureStore'
 
 import App from './components/AppContainer'
 
-// optinally provide persisted state
-// to override the intitial state from reducers
-const persistedState = {
-    count: 0
-}
-
-let store = configure(persistedState)
+const store = configure()
 
 render((
     <Provider store={store}>
