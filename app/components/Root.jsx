6dd79de051484781b430cf6c39c79a2229567d9f
Johan Nyström <johan_n694@hotmail.com>
2016-06-08 17:52:26 +0200

    moved top level app configuration to root component

---
+++ app/components/Root.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Provider } from 'react-redux'
+import { Router, Route, browserHistory } from 'react-router'
+
+const Root = ({store}) => (
+    <Provider store={store}>
+        <Router history={browserHistory}>
+            <Route path="/" component={App} />
+        </Router>
+    </Provider>
+)
+
+export default Root
+
--- app/index.jsx
+++ app/index.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
-import { Router, Route, browserHistory } from 'react-router'
 import { render } from 'react-dom'
-import { Provider } from 'react-redux'
 import configure from './store/configureStore'
 
 import App from './components/AppContainer'
@@ -9,9 +7,5 @@ import App from './components/AppContainer'
 const store = configure()
 
 render((
-    <Provider store={store}>
-        <Router history={browserHistory}>
-            <Route path="/" component={App} />
-        </Router>
-    </Provider>
+    <Root store={store} />
 ), document.getElementById('app'))
