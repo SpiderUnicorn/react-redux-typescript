c0e35dfada0b7b9ab557df7ae423fe56dd71dd3c
Johan Nyström <johan_n694@hotmail.com>
2016-06-08 10:38:31 +0200

    refactored to object expression

--- app/actions/actions.js
+++ app/actions/actions.js
@@ -1,9 +1,9 @@
 import { INCREMENT, DECREMENT } from '../constants/actionTypes'
 
-export const increment = () => (
-    { type: INCREMENT }
-)
+export const increment = () => ({
+    type: INCREMENT
+})
 
-export const decrement = () => (
-    { type: DECREMENT }
-)
+export const decrement = () => ({
+    type: DECREMENT
+})
--- app/components/AppContainer.jsx
+++ app/components/AppContainer.jsx
@@ -2,22 +2,18 @@ import { increment, decrement } from '../actions/actions'
 import {connect} from 'react-redux'
 import App from './App'
 
-const mapStateToProps = (state) => {
-    return {
-        value: state.count
-    }
-}
+const mapStateToProps = (state) => ({
+    value: state.count
+})
 
-const mapDispatchToProps = (dispatch) => {
-    return {
-        onIncrement: () => {
-            dispatch(increment())
-        },
-        onDecrement: () => {
-            dispatch(decrement())
-        }
+const mapDispatchToProps = (dispatch) => ({
+    onIncrement: () => {
+        dispatch(increment())
+    },
+    onDecrement: () => {
+        dispatch(decrement())
     }
-}
+})
 
 const AppContainer = connect(
     mapStateToProps,
