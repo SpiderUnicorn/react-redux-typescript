19d1c9a243d65d90e5c85c51df6d3b07a4f0acef
Johan Nyström <johan_n694@hotmail.com>
2016-06-10 15:24:57 +0200

    added production env variable for build

--- webpack.config.js
+++ webpack.config.js
@@ -61,6 +61,10 @@ switch(process.env.npm_lifecycle_event) {
             {
                 devtool: 'source-map'
             },
+            parts.setFreeVariable(
+                'process.env.NODE_ENV',
+                'production'
+            ),
             parts.minify(),
             parts.setupCSS(PATHS.app)
         );
