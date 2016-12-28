db94834da5e091c1008b5aae62be72f0f353f1e7
Johan Nyström <johan_n694@hotmail.com>
2016-12-28 20:30:03 +0100

    added typings

---
+++ typings/index.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="globals/redux/index.d.ts" />
+/// <reference path="modules/react-dom/index.d.ts" />
+/// <reference path="modules/react-redux/index.d.ts" />
+/// <reference path="modules/react-router/index.d.ts" />
+/// <reference path="modules/react/index.d.ts" />
