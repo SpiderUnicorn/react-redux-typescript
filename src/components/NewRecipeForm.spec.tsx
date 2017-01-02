280a0168cfecc47f7948c02dc16fd9c54518caf6
Johan Nyström <johan_n694@hotmail.com>
2017-01-02 18:06:04 +0100

    example of testing form submit

--- src/components/NewRecipeForm.spec.tsx
+++ src/components/NewRecipeForm.spec.tsx
@@ -1,27 +1,62 @@
 import 'mocha'
 import React from 'react'
 import expect from 'expect'
-import { mount, shallow } from 'enzyme'
+import { mount, shallow, ShallowWrapper } from 'enzyme'
 
 import NewRecipeForm, {IProps} from './NewRecipeForm'
 
-function setup() {
+let callCount = 0;
+function setup(): ShallowWrapper<any, any> {
    const props: IProps = {
-      submit: () => {}
+      submit: () => {callCount++}
    }
 
    return shallow(<NewRecipeForm {...props} />)
 }
 
 describe("NewRecipeForm", () => {
+    let wrapper;
+    let form;
+  
+    beforeEach(() => {
+       wrapper = setup()
+       form = wrapper.find('form')
+    })
+
     it('renders the form', () => {
-      const wrapper = setup();
-      expect(wrapper.find('form').length).toEqual(1);
+      expect(form.length).toEqual(1);
     })
 
     it('picks up title and description', () => {
-      const wrapper = setup();
-   
-      expect(wrapper.find('form').length).toEqual(1);
+      wrapper.find('#recipeTitle').simulate('change', {target: {value: 'Test title'}});
+      wrapper.find('#recipeDescription').simulate('change', {target: {value: 'Test description'}});
+      
+      expect(wrapper.state()).toEqual({
+        title: "Test title",
+        description: "Test description"
+      })
+    })
+
+    it('clears the form on submit', () => {
+      wrapper.find('#recipeTitle').simulate('change', {target: {value: 'Test title'}});
+      wrapper.find('#recipeDescription').simulate('change', {target: {value: 'Test description'}});
+      
+      wrapper.find('button').simulate('click', { preventDefault() {} });
+
+      expect(wrapper.state()).toEqual({
+        title: "",
+        description: ""
+      })
+    })
+
+    it('calls passed in submit function on submit', () => {
+      callCount = 0;
+    
+      wrapper.find('#recipeTitle').simulate('change', {target: {value: 'Test title'}});
+      wrapper.find('#recipeDescription').simulate('change', {target: {value: 'Test description'}});
+      
+      wrapper.find('button').simulate('click', { preventDefault() {} });
+
+      expect(callCount).toBe(1);
     })
 })
